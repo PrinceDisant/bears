e714cecbde0f473b087042401267820b7eabfa55
Disant Upadhyay <79222290+PrinceDisant@users.noreply.github.com>
2021-09-08 14:20:01 -0230

    navbar initialization

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './navbar.css'
+
+function Navbar() {
+    return (
+        <div className="navbar-wrapper">
+            
+        </div>
+    )
+}
+
+export default Navbar
