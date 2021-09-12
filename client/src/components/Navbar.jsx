794cbd9a3f465db7d1206a410a6f25819cf48dc6
Disant Upadhyay <79222290+PrinceDisant@users.noreply.github.com>
2021-09-12 09:20:05 -0230

    Navbar initial changes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import './navbar.css'
+import './navbar.css';
+// import { Link } from "react-router-dom";
 
 function Navbar() {
     return (
-        <div className="navbar-wrapper">
-            
+        <div className="navbar">
+            <div className="left">
+                <h2>DASHBOARD</h2>
+            </div>
+            <div className="right">
+                <i class="fas fa-search"></i>
+            </div>
         </div>
     )
 }
